b1723601aca43adfe56607ece53733eff5d6e72b
AaronLocatis <aalocatis2@gmail.com>
2022-08-23 11:46:00 -0500

    styled footer, layout page complete

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className="navbar-container">
+            <p className="logo">
+                <Link href="/">Pioneer Subwoofer</Link>
+            </p>
+            <button type="button"
+                className="cart-icon" onClick="">
+                <AiOutlineShopping />
+                <span classNam="cart-item-qty"></span>
+            </button>
+        </div>
     )
 }
 
