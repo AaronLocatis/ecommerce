18ab76118cc3acbc4e264d9c4614f2237a09d0e1
AaronLocatis <aalocatis2@gmail.com>
2022-08-24 13:13:26 -0500

    [slug] product details component

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
 import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
@@ -20,7 +22,7 @@ const ProductDetails = ({ product, products }) => {
                             />
                         ))} */}
                 </div >
-                <div className="product-details-desc">
+                <div className="product-detail-desc">
                     <h1>{name}</h1>
                     <div className="reviews">
                         <div>
@@ -37,9 +39,40 @@ const ProductDetails = ({ product, products }) => {
                     <h4>Details: </h4>
                     <p>{details}</p>
                     <p className="price">${price}</p>
-
+                    <div className="quantity">
+                        <h3>Quantity:</h3>
+                        <p className="quantity-desc">
+                            <span className="minus"
+                                onClick=""><AiOutlineMinus />
+                            </span>
+                            <span className="num"
+                                onClick="">0</span>
+                            <span className="plus"
+                                onClick=""><AiOutlinePlus />
+                            </span>
+                        </p>
+                    </div>
+                    <div className="buttons">
+                        <button type="button"
+                            className="add-to-cart"
+                            onClick="">Add to Cart</button>
+                        <button type="button"
+                            className="buy-now" onClick="">Buy Now</button>
+                    </div>
                 </div>
             </div >
+            <div
+                className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
+                        {products.map((item) => (
+                            <Product key={item._id}
+                                product={item} />
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div >
     )
 }
