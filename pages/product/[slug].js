b16668618118a8e34aa57af0527197ba5847d9b3
AaronLocatis <aalocatis2@gmail.com>
2022-08-28 13:12:59 -0500

    no changes

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -118,3 +118,4 @@ export const getStaticProps = async ({ params: { slug } }) => {
 }
 
 export default ProductDetails
+
--- pages/success.js
+++ pages/success.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 
 import { useStateContext } from '../context/StateContext';
+import { runFireworks } from '../lib/utils';
 
 const success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
