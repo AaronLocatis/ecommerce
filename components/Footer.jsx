9e13cd5f21d4e5167bfd660d917edfd4772a891c
AaronLocatis <aalocatis2@gmail.com>
2022-08-29 12:57:46 -0500

    footer.js changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
     return (
         <div className="footer-container">
-            <p>2022 Aaron's Boomin Bass All Right Reserved</p>
+            <p>2022 Aaron&apos;s Boomin Bass All Right Reserved</p>
             <p className="icons">
                 <AiFillInstagram />
                 <AiOutlineTwitter />
