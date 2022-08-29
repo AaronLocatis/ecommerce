6d5a1a4891d2fb5b4be146d40cb859304be8c10e
AaronLocatis <aalocatis2@gmail.com>
2022-08-29 12:47:02 -0500

    Succes.js changes

--- pages/success.js
+++ pages/success.js
@@ -24,8 +24,11 @@ const Success = () => {
                 </p>
                 <h2>Thank you for your order!</h2>
                 <p className="email-msg">Check your email inbox for the receipt.</p>
-                <p className="description">If you have any questions, please email
-                    <a className="email" href="mailto:order@example.com"></a>
+                <p className="description">
+                    If you have any questions, please email
+                    <a className="email" href="mailto:order@example.com">
+                        order@example.com
+                    </a>
                 </p>
                 <Link href="/">
                     <button type="button" width="300px" className="btn">
